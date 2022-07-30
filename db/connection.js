42e9e21f3fb36e2031b4e03629c233d54224bf1a
MirMello <mirandaleeallen@gmail.com>
2022-07-29 19:16:52 -0600

    removed my password

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: 'May4@2017mjm',
+    password: '',
     database: 'election'
   });
 
